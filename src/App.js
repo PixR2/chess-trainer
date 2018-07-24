5ded22b25b892066c24c1cac8ba359ec28eecda8
Johannes Radmer <j.radmer@pyramics.com>
2018-07-24 16:51:50 +0200

    Added basic piece movement and chess.js.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Chess from 'react-chess'
+import ChessBoard from 'react-chess'
 import { Container } from 'semantic-ui-react'
+var Chess = require('chess.js');
+var chess = new Chess();
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { pieces: ChessBoard.getDefaultLineup() }
+    this.handleMovePiece = this.handleMovePiece.bind(this)
+  }
+
+  handleMovePiece(piece, fromSquare, toSquare) {
+    const newPieces = this.state.pieces
+      .map((curr, index) => {
+        if (piece.index === index) {
+          return `${piece.name}@${toSquare}`
+        } else if (curr.indexOf(toSquare) === 2) {
+          return false // To be removed from the board
+        }
+        return curr
+      })
+      .filter(Boolean)
+
+    this.setState({ pieces: newPieces })
+  }
+
   render() {
+    const {pieces} = this.state
     return (
-      <main style={{maxWidth: '90vmin', margin: '1vmin auto'}}>
-        <Chess />
+      <main style={{ maxWidth: '90vmin', margin: '1vmin auto' }}>
+        <ChessBoard pieces={pieces} onMovePiece={this.handleMovePiece} />
       </main>
     );
   }
